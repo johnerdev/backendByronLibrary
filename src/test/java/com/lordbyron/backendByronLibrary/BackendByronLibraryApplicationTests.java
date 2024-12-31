dd51e4eea3cdf3001fd91578a54cb4671ef7faad
Johson Rodriguez <sukerjc@gmail.com>
2024-12-30 19:34:50 -0500

    ok

---
+++ src/test/java/com/lordbyron/backendByronLibrary/BackendByronLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lordbyron.backendByronLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendByronLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
